e490caf7d687a8adc2bff693153c646ce8cceabb
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-07-16 22:43:11 +0000

    validation and modalform

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -2,17 +2,18 @@ class KnowledgesController < ApplicationController
     def index
       @knowledge = Knowledge.order("RANDOM()").first
     end
-    def new
-      @knowledge = Knowledge.new
-    end
+    
 
     def create
-      Knowledge.create(knowledge_params)
+      @knowledge = Knowledge.create(knowledge_params)
       if @knowledge.invalid?
         flash[:error] = '<strong> could not save</strong> The data you entered is invalid.'
       end
       redirect_to root_path
     end
+    def about
+    end
+
 
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
    root 'knowledges#index'
    resources :knowledges
+   get 'about', to: 'knowledges#about'
 end
