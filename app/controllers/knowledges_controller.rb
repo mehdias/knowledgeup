4692cff1c776ee269950eb61af6b5c03b6d228fa
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-07-16 01:50:37 +0000

    simpleform

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -2,4 +2,7 @@ class KnowledgesController < ApplicationController
     def index
       @knowledge = Knowledge.order("RANDOM()").first
     end
+    def new
+      @knowledge = Knowledge.new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
    root 'knowledges#index'
+   resources :knowledges
 end
