3a9a56413111cdcd693b242fcdc4c6703d926122
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-07-16 05:55:32 +0000

    form and validations

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -5,4 +5,19 @@ class KnowledgesController < ApplicationController
     def new
       @knowledge = Knowledge.new
     end
+
+    def create
+      Knowledge.create(knowledge_params)
+      if @knowledge.invalid?
+        flash[:error] = '<strong> could not save</strong> The data you entered is invalid.'
+      end
+      redirect_to root_path
+    end
+
+    private
+
+    def knowledge_params
+      params.require(:knowledge).permit(:title, :description, :written_by)
+    end
+
 end
