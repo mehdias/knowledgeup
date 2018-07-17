a3e8152aa69209a3b50df55822bf244745d5e484
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-07-17 00:41:35 +0000

    final touches

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -1,5 +1,5 @@
 class Knowledge < ApplicationRecord
   validates :title, presence: true, length: { maximum: 80, minimum: 5}
-  validates :description, presence: true, length: { maximum: 500, minimum: 10}
+  validates :description, presence: true, length: { maximum: 650, minimum: 10}
   validates :written_by, presence: true, length: {maximum: 30, minimum: 3}
 end
